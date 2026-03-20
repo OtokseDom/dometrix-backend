e7c49484ed9d281f60181d1c2f7e6b2bfc254418
Dominic Escoto <128564959+OtokseDom@users.noreply.github.com>
2026-03-20 09:55:58 +0400

    Update(Role): Relationship to org

--- backend/app/Domain/Role/Models/Role.php
+++ backend/app/Domain/Role/Models/Role.php
@@ -2,8 +2,12 @@
 
 namespace App\Domain\Role\Models;
 
+use App\Domain\Organization\Models\Organization;
+use App\Domain\Organization\Models\OrganizationUser;
+use App\Domain\User\Models\User;
 use App\Traits\UsesUuid;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Role extends Model
@@ -18,4 +22,27 @@ class Role extends Model
     protected $casts = [
         'permissions' => 'array',
     ];
+
+//    Relationship
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(
+            User::class,
+            'organization_user'
+        )
+            ->using(OrganizationUser::class)
+            ->withPivot('organization_id')
+            ->withTimestamps();
+    }
+
+    public function organizations(): BelongsToMany
+    {
+        return $this->belongsToMany(
+            Organization::class,
+            'organization_user'
+        )
+            ->using(OrganizationUser::class)
+            ->withPivot('user_id')
+            ->withTimestamps();
+    }
 }
